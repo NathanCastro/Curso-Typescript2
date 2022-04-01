f645d84f968fe3532aae4f5f5ab708ab1eeec6fb
Nathan Castro <nathanslc18@gmail.com>
2022-04-01 06:19:32 -0300

    chore(typescript2):finish project

---
+++ dist/js/enums/dias-da-semana.js
@@ -0,0 +1,15 @@
+export var DiaDaSemana;
+(function (DiaDaSemana) {
+    DiaDaSemana[DiaDaSemana["DOMINGO"] = 0] = "DOMINGO";
+    DiaDaSemana[DiaDaSemana["SEGUNDA"] = 1] = "SEGUNDA";
+    DiaDaSemana[DiaDaSemana["TERCA"] = 2] = "TERCA";
+    DiaDaSemana[DiaDaSemana["QUARTA"] = 3] = "QUARTA";
+    DiaDaSemana[DiaDaSemana["QUINTA"] = 4] = "QUINTA";
+    DiaDaSemana[DiaDaSemana["SEXTA"] = 5] = "SEXTA";
+    DiaDaSemana[DiaDaSemana["SABADO"] = 6] = "SABADO";
+    DiaDaSemana[DiaDaSemana["A"] = 7] = "A";
+    DiaDaSemana[DiaDaSemana["B"] = 3] = "B";
+    DiaDaSemana[DiaDaSemana["C"] = 4] = "C";
+    DiaDaSemana[DiaDaSemana["D"] = 5] = "D";
+    DiaDaSemana[DiaDaSemana["F"] = 6] = "F";
+})(DiaDaSemana || (DiaDaSemana = {}));
